d6de7e546c52916a007c640083141796d39aefd2
belezabeth <belezabeth3@gmail.com>
2025-05-12 14:45:20 +0700

    First Commit

---
+++ Assets/Code/Inventory/Item.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "RumpledCode/Item", order = 1)]
+public class Item : ScriptableObject
+{
+    public string id;
+    public string description;
+    public Sprite icon;
+    public GameObject prefab;
+
+    [Header("Shop Info")]
+    public int price = 10;
+}
